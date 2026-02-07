3f9a6ecda023a1a622ff6c3e78ce54f1e49e81fe
Sujay Mukherjee <sujaymukherjee19111994@gmail.com>
2026-02-07 19:50:20 +0530

    Updates

--- _next/static/bkccDPI9TTlvsgAhLC_OD/_buildManifest.js
+++ _next/static/bkccDPI9TTlvsgAhLC_OD/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/antigravity//_next/:path+",
-        "destination": "/antigravity/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
